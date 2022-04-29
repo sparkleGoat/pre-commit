46ad7061b9948fb21730dc5243d0e76487b5065f
Mary Rogers <maryrogers415@gmail.com>
2022-04-29 12:04:14 -0400

    simplify script

--- hooks/helm-unittest.sh
+++ hooks/helm-unittest.sh
@@ -3,30 +3,11 @@
 set -e
 export PATH=$PATH:/usr/local/bin
 
-exit_status=0
-enable_list=""
-
-parse_arguments() {
-	while (($# > 0)); do
-		# Grab param and value splitting on " " or "=" with parameter expansion
-		local PARAMETER="${1%[ =]*}"
-		local VALUE="${1#*[ =]}"
-		if [[ "$PARAMETER" == "$VALUE" ]]; then VALUE="$2"; fi
-		shift
-			enable_list="$enable_list $VALUE"
-
-	done
-	enable_list="${enable_list## }" # remove preceeding space
-}
-
-parse_arguments "$@"
-
-
 if which helm &> /dev/null $? != 0 ; then
     echo "HELM must be installed"
     exit 1
 fi
 
-helm unittest ${enable_list:+ --enable="$enable_list"}
+helm unittest "$@"
 
-exit $exit_status
+exit 0
